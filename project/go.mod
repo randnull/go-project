3be859921eccbee21794c3d897cf290be50047d3
Кирилл Горюнов <kirillgorunov@MacBook-Pro-Kirill.local>
2023-12-17 00:09:41 +0300

    add

--- project/go.mod
+++ project/go.mod
@@ -2,4 +2,9 @@ module project
 
 go 1.15
 
-require go.mongodb.org/mongo-driver v1.13.1
+require (
+	github.com/jmoiron/sqlx v1.3.5 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/pressly/goose/v3 v3.17.0 // indirect
+	go.mongodb.org/mongo-driver v1.13.1
+)
--- project/internal/location/repository/repository.go
+++ project/internal/location/repository/repository.go
@@ -1,4 +1,29 @@
 package repository
 
+import (
+	"context"
+	"github.com/jmoiron/sqlx"
+	"log"
+)
+
 type LocationRepository struct {
+	db *sqlx.DB
+}
+
+func NewLocationRepository() *LocationRepository {
+	db, err := sqlx.Open("postgres", "postgres://postgres:admin@localhost:5432/driver?sslmode=disable")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.PingContext(context.Background())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &LocationRepository{
+		db: db,
+	}
 }
